d48e1e42a0d94c8181b2914137c0087f0f2419f3
CNelson2004 <cnelson1845@gmail.com>
2025-01-30 13:36:03 -0700

    Dao's completed. (Untested)

--- server/src/main/java/dataaccess/AuthDao.java
+++ server/src/main/java/dataaccess/AuthDao.java
@@ -1,12 +1,47 @@
 package dataaccess;
 
-public interface AuthDao {
-    //void clear()
-    //authToken createAuth()
-    //authToken getAuth()
-    //updateAuth?
-    //void deleteAuth()
+import model.AuthData;
+import model.UserData;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+//Do Dao's, and write tests for them, and make htem work, then tackle one endpoint at a time in the service section
+interface AuthDao {
+    void clear();
+    AuthData createAuth(UserData user);
+    AuthData getAuth(AuthData auth);
+    void deleteAuth(AuthData auth);
 }
 
 class MemoryAuthDao implements AuthDao{
+    private final ArrayList<AuthData> authTokens = new ArrayList<>();
+
+    public void clear(){
+        authTokens.clear();
+    }
+
+    public AuthData createAuth(UserData user) {
+        //Creates an AuthData(Authtoken) and adds it to the database.
+        String token = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(token,user.name());
+        authTokens.add(authData);
+        return authData;
+    }
+
+    public AuthData getAuth(AuthData auth){
+        for(AuthData token: authTokens){
+            if(token.authToken().equals(auth.authToken())){
+                return token;
+            }
+        }
+        return null;
+        //Throw nullError saying that token does not exist in database
+    }
+
+    public void deleteAuth(AuthData auth){
+        //Check that auth exists first and throw error if it does not
+        authTokens.removeIf(token -> token.authToken().equals(auth.authToken()));
+        //^For-each loop w/ if statement inside
+    }
 }
--- server/src/main/java/dataaccess/GameDao.java
+++ server/src/main/java/dataaccess/GameDao.java
@@ -1,14 +1,61 @@
 package dataaccess;
 
+import chess.ChessGame;
+import model.UserData;
+import model.GameData;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.UUID;
+
 public interface GameDao {
-    //void clear()
-    //ChessGame createGame()
-    //ChessGame getGame()
-    //Collection<GameData> getAllGames()
-    //updateGame?
-    //deleteGame?
-    //Collection<GameData> listGames() /*retrieves all games*/
+    void clear();
+    GameData createGame(UserData user, String gameName, String color);
+    GameData getGame(GameData game);
+    Collection<GameData> getAllGames();
+    GameData updateGame(GameData game, String color, UserData user);
+    void deleteGame(GameData game);
 }
 
 class MemoryGameDao implements GameDao{
+    private final ArrayList<GameData> games = new ArrayList<>();
+
+    public void clear(){
+        games.clear();
+    }
+
+    public GameData createGame(UserData user, String gameName, String color) {
+        int gameID = UUID.randomUUID().hashCode();
+        GameData game = new GameData(user.name(),null,gameName,gameID,new ChessGame());
+        if(color.equals("BLACK")){game = new GameData(null,user.name(),gameName,gameID,new ChessGame());}
+        return game;
+    }
+
+    public GameData getGame(GameData game){
+        for(GameData current: games){
+            if(current.gameID() == game.gameID()){
+                return current;
+            }
+        }
+        return null;
+        //Throw nullError saying that token does not exist in database
+    }
+
+    public void deleteGame(GameData game){
+        //Check that auth exists first and throw error if it does not
+        games.removeIf(current -> current.gameID() == game.gameID());
+    }
+
+    public Collection<GameData> getAllGames(){
+        return games;
+    }
+
+    public GameData updateGame(GameData game, String color, UserData user){
+        //Creates a new GameData and deletes the old one
+        deleteGame(game);
+        GameData updatedGame = new GameData(game.whiteUsername(),user.name(),game.gameName(),game.gameID(),game.game());
+        if(color.equals("WHITE")){updatedGame = new GameData(user.name(),game.blackUsername(),game.gameName(),game.gameID(),game.game());}
+        games.add(updatedGame);
+        return updatedGame;
+    }
 }
